64c26da4745b9bbf5baa48279e2034c1168f13e7
bruch-alex <alex.bruch@tutanota.com>
2025-03-24 10:29:38 +0100

    init

---
+++ src/main/java/alex/bruch/tripsharing/TripSharingApplication.java
@@ -0,0 +1,13 @@
+package alex.bruch.tripsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alex/bruch/tripsharing/TripSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.bruch.tripsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
