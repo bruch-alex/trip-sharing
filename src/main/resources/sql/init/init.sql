d900f003c8931dc2edab2a4f26b5992ea74923ad
bruch-alex <alex.bruch@tutanota.com>
2025-04-12 13:47:05 +0200

    split users table into 2 tables

--- src/main/resources/sql/init/init.sql
+++ src/main/resources/sql/init/init.sql
@@ -1,17 +1,23 @@
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE IF NOT EXISTS user_logins
 (
     id           SERIAL PRIMARY KEY,
     email        TEXT NOT NULL UNIQUE CHECK (email ~ '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'),
     phone_number TEXT NOT NULL UNIQUE, --TODO: add regex check
     password     TEXT NOT NULL,        --TODO: add length check
+    created_at   TIMESTAMP DEFAULT NOW(),
+    updated_at   TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS user_profiles
+(
+    id INTEGER PRIMARY KEY,
     first_name   TEXT NOT NULL CHECK (LENGTH(first_name) >= 2),
     middle_name  TEXT CHECK (LENGTH(middle_name) >= 2),
     last_name    TEXT NOT NULL CHECK (LENGTH(last_name) >= 2),
-    created_at   TIMESTAMP DEFAULT NOW(),
-    updated_at   TIMESTAMP
+    FOREIGN KEY (id) REFERENCES user_logins (id)
 );
 
 CREATE OR REPLACE FUNCTION update_modified_column()
