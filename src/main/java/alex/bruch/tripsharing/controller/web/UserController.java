925a8060379fd185a574912d5ecee48d72a09740
bruch-alex <alex.bruch@tutanota.com>
2025-04-12 14:55:21 +0200

    add register page and user controller

--- src/main/java/alex/bruch/tripsharing/controller/web/UserController.java
+++ src/main/java/alex/bruch/tripsharing/controller/web/UserController.java
@@ -3,6 +3,8 @@ package alex.bruch.tripsharing.controller.web;
 import alex.bruch.tripsharing.service.impl.CustomUserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +17,14 @@ public class UserController {
         this.customUserDetailsService = customUserDetailsService;
     }
 
-    public String getRegisterPage(Model model){
+    @GetMapping("/register")
+    public String getRegisterPage(){
+        return "register-page";
+    }
 
+    @PostMapping("/register")
+    public String postRegister(Model model){
+        model.addAttribute("user", user);
+        return "register-success";
     }
 }
